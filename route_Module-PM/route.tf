d0552b488a2d8cc2f9005e88587c933270759bfc
pranay168 <98884571+pranay168@users.noreply.github.com>
2022-02-24 22:02:58 +0530

    route module

---
+++ route_Module-PM/route.tf
@@ -0,0 +1,56 @@
+#provider information
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAWI7TGBPHBVNDQAN7"
+  secret_key = "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"
+}
+
+#vpc creation
+
+resource "aws_vpc" "amazonvpc" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    name = "amazon"
+  }
+}
+#creating IGW
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.amazonvpc.id
+}
+# Create a subnet
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.amazonvpc.id
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "subnet"
+  }
+}
+
+#creating security group
+resource "aws_security_group" "allow_ssh" {
+  vpc_id = aws_vpc.amazonvpc.id
+}
+
+#creating routetable
+resource "aws_route_table" "route-public" {
+  vpc_id = aws_vpc.amazonvpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+
+  tags = {
+    Name = "public-route-table-demo"
+  }
+}
+
+resource "aws_route_table_association" "main" {
+  subnet_id      = aws_subnet.main.id
+  route_table_id = aws_route_table.route-public.id
+}
+
+
+  
