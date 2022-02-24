f9e09a2439ee7182a086e8098087e2d470beb829
eben-ismile <98841896+eben-ismile@users.noreply.github.com>
2022-02-24 18:02:37 +0000

    Update security_group.tf

--- security_Module-PM/security_group.tf
+++ security_Module-PM/security_group.tf
@@ -1,8 +1,7 @@
 # Configure the AWS Provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAWI7TGBPHBVNDQAN7"
-  secret_key = "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"
+
 }
 
 # Create a VPC
